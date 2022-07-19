fe99bddbaa76b32b8d55d6123afe491562dc2c02
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-07-19 16:18:00 -0300

    proyecto terminado

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-// esto sirve para cambiar el idioma
-let check = document.querySelector(".check");
-check.addEventListener("click", idioma);
+// // esto sirve para cambiar el idioma
+// let check = document.querySelector(".check");
+// check.addEventListener("click", idioma);
 
-function idioma() {
-  let id = check.checked;
-  if (id == true) {
-    location.replace("./html/indexx.html");
-  } else {
-    location.replace("../index.html");
-  }
-}
+// function idioma() {
+//   let id = check.checked;
+//   if (id == true) {
+//     location.replace("./html/indexx.html");
+//   } else {
+//     location.replace("../index.html");
+//   }
+// }
 
 // delacando variables btc
 const btc = document.querySelector("#btc");
@@ -77,3 +77,46 @@ async function getResponseD() {
   }).format(parseNumber1);
 }
 getResponseD();
+
+//declarando variables  Dog
+const dog = document.querySelector("#dog");
+const mkDog = document.querySelector("#marketcap-dog");
+const sDog = document.querySelector(".simbole-dog");
+
+async function getResponseDog() {
+  const response = await fetch("https://api.coinlore.net/api/ticker/?id=2");
+  const data = await response.json(); // Extracting data as a JSON Object from the response
+  const parseNumber1 = data[0].price_usd;
+  sDog.innerHTML = data[0].symbol;
+  const parseNumber = data[0].market_cap_usd;
+  mkDog.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(parseNumber);
+  dog.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(parseNumber1);
+}
+getResponseDog();
+//declarando variables  bnb
+const bnb = document.querySelector("#bnb");
+const mkBnb = document.querySelector("#marketcap-bnb");
+const sBnb = document.querySelector(".simbole-bnb");
+
+async function getResponseBnb() {
+  const response = await fetch("https://api.coinlore.net/api/ticker/?id=2710");
+  const data = await response.json(); // Extracting data as a JSON Object from the response
+  const parseNumber1 = data[0].price_usd;
+  sBnb.innerHTML = data[0].symbol;
+  const parseNumber = data[0].market_cap_usd;
+  mkBnb.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(parseNumber);
+  bnb.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(parseNumber1);
+}
+getResponseBnb();
